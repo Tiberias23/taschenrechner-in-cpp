1dc7a41927b969b998a1a7e68943b8cb5d5451ce
Lupo <tiberias.peters+GitKraken@gmail.com>
2025-03-05 20:04:50 +0100

    taschenrechner verbessert
    Mathematische Funktionen entfernt und direkt ersetzt
    
    Die Funktionen für Aditionen, Subtraktionen, Multiplikationen, Divisionen, Potenzen und Quadratwurzeln wurden entfernt. Die Berechnungen werden nun direkt im `main()`-Block mit den entsprechenden Operatoren und Standardbibliotheksfunktionen durchgeführt.
    
    Mathematische Funktionen entfernt und ersetzt
    
    Die Funktionen für Aditionen, Subtraktionen, Multiplikationen, Divisionen, Potenzen und Quadratwurzeln wurden entfernt. Die Berechnungen werden nun direkt im `main()`-Block mit den entsprechenden Operatoren und Standardbibliotheksfunktionen durchgeführt.

--- taschenrechner/taschenrechner.cpp
+++ taschenrechner/taschenrechner.cpp
@@ -2,30 +2,6 @@
 #include <cmath>
 using namespace std;
 
-double Adieren(double a, double b) {
-    return a + b;
-}
-
-double subtrairen(double a, double b){
-    return a - b;
-}
-
-double Multieplizieren(double a, double b) {
-    return a * b;
-}
-
-double Diviedieren(double a, double b){
-    return a / b;
-}
-
-double Potenzen(double base, double exponent) {
-    return pow(base, exponent);
-}
-
-double Quadratwurzel(double a) {
-    return sqrt(a);
-}
-
 int main() {
     cout << "Was willst du machen?" << endl;
     cout << "Adieren(1)\nsubtrairen(2)\nmultieplizieren(3)\ndiviedieren(4)\nPotenzen(5)\nquadratwurzel(6)" << endl;
@@ -40,7 +16,7 @@ int main() {
         cin >> a;
         cout << "Was ist die zweite Zahl" << endl;
         cin >> b;
-        cout << Adieren(a, b);
+        cout << a + b;
         break;
     }
     case 2:
@@ -49,7 +25,7 @@ int main() {
         cin >> a;
         cout << "Was ist die zweite Zahl" << endl;
         cin >> b;
-        cout << subtrairen(a, b);
+        cout << a - b;
         break;
     case 3:
         // Multiplizieren
@@ -57,7 +33,7 @@ int main() {
         cin >> a;
         cout << "Was ist die zweite Zahl" << endl;
         cin >> b;
-        cout << Multieplizieren(a, b);
+        cout << a * b;
         break;
     case 4:
         // Dividieren
@@ -65,7 +41,7 @@ int main() {
         cin >> a;
         cout << "Was ist die zweite Zahl" << endl;
         cin >> b;
-        cout << Diviedieren(a, b);
+        cout << a / b;
         break;
     case 5:
         // Potenzen
@@ -73,13 +49,13 @@ int main() {
         cin >> a;
         cout << "Was ist der Exponent" << endl;
         cin >> b;
-        cout << Potenzen(a, b);
+        cout << pow(a, b);
         break;
     case 6:
         // Quadratwurzel
         cout << "Was ist die Zahl" << endl;
         cin >> a;
-        cout << Quadratwurzel(a);
+        cout << sqrt(a);
         break;
     default:
         cout << "Ungültige Auswahl";
